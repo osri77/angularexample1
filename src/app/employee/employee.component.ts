8db507e192fe01a8cd9fecc83c8a71df501295bb
LocalAdmin <osri77@yahoo.com>
2018-03-23 14:35:02 +1000

    Added the logic to unsubscribe to observable

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/operator/retry';
 import 'rxjs/add/operator/retryWhen';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/scan';
+import {ISubscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-employee',
@@ -25,10 +26,11 @@ export class EmployeeComponent implements OnInit {
   constructor(private _employeeService:EmployeeService, private _activatedRoute:ActivatedRoute, private _router:Router) { }
   employee: Iemployee;
   statusMessage: string = 'Loading data. Please wait...';
+  subscription: ISubscription;
   ngOnInit() {
     let empCode: string = this._activatedRoute.snapshot.params['code'];
 
-    this._employeeService.getEmployeeByCode(empCode)
+    this.subscription = this._employeeService.getEmployeeByCode(empCode)
     .retryWhen((err) => {
       return err.scan((retryCount, val) => {
           retryCount += 1;
@@ -40,7 +42,7 @@ export class EmployeeComponent implements OnInit {
               throw (err);
           }
       }, 0).delay(1000)
-  })
+      })
 
             .subscribe((employeeData) => {
                 if (employeeData == null) {
@@ -71,6 +73,11 @@ toggleDetails(): void {
 
 onBackButtonClick():void{
   this._router.navigate(["/employees"]);
+}
+
+onCancelButtonClick():void{
+  this.subscription.unsubscribe();
+  this.statusMessage = "Request Cancelled";
 }
 
 
